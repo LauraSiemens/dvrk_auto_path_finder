943621545f77feccf90c8b6ffdd229d27a464f08
LauraSiemens <lauragsiemens@gamil.com>
2026-04-18 15:19:37 -0700

    fix save images

--- get_disparity.py
+++ get_disparity.py
@@ -8,9 +8,4 @@ def get_disparity(left_img, right_img):
         - left_img(uint8): the left rgb image
         - right_img(uint8): the right rgb image
     """ 
-    save_images(left_img, right_img)
     run_model.run_model("image_pngs/left_image.png", "image_pngs/right_image.png") # generates map and outputs to raftstereo/demo_output/images.npy
-
-def save_images(left_img, right_img):
-    cv2.imwrite("image_pngs/left.png", left_img)
-    cv2.imwrite("images_pngs/right.png", right_img)
